8991e23fab630b0f5cdf936fc4f05b96540582c5
Reyes <pohyd02@gmail.com>
2025-12-06 10:15:13 +0800

    Local Project Porting to Github

---
+++ backend/endpoints/register.endpoint.js
@@ -0,0 +1,50 @@
+// import Router to handle endpoints subrouting
+const app = require("express").Router();
+
+// import supabase API
+const { createClient } = require("@supabase/supabase-js");
+const supabaseUrl = 'https://bpjmdrzhirsebupwxgbp.supabase.co';
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+app.post("/register", async function (request, response) {
+    console.log(request.body);
+    const username = request.body["Username"];
+    const password = request.body["Password"];
+
+    await supabase.from('users').select('*').eq("username", username).then(async function (selectQueryResult) {
+        if (selectQueryResult["error"] != null) { // database error
+            response.status(500).json({
+                "message": "Error. Try Again!",
+            });
+        } else if (selectQueryResult["data"].length > 0) { // duplicate username found
+            response.status(500).json({
+                "message": "Error. Username already exists.",
+            });
+        } else if (selectQueryResult["data"].length == 0) { // duplicate username NOT found
+            await supabase
+                .from('users')
+                .insert([
+                    {
+                        "username": username,
+                        "display_name": username,
+                        "password": password,
+                    }
+                ])
+                .select().then(function (insertQueryResult) {
+                    if (insertQueryResult["error"] != null) { // database error
+                        response.status(500).json({
+                            "message": "Error, try again!",
+                        });
+                    } else { // good to go
+                        console.log(insertQueryResult);
+                        response.status(200).json({
+                            "message": "Account created successfully.",
+                        });
+                    }
+                });
+        } 
+    });
+});
+
+module.exports = app;
---
+++ backend/server.js
@@ -0,0 +1,51 @@
+// Core handler file to handle different endpoints
+
+//import dotenv
+const dotenv = require("dotenv");
+dotenv.config();
+
+// import express.js
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+// import routes (endpoints)
+const registerRoute = require("./endpoints/register.endpoint.js");
+
+// import supabase API
+const { createClient } = require("@supabase/supabase-js");
+const supabaseUrl = 'https://bpjmdrzhirsebupwxgbp.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// use routes (endpoints)
+app.use(registerRoute);
+
+app.get("/test/:uuid", async (request, response) => {
+    const uuid = request.params.uuid;
+    await supabase.from('users').select('*').eq("id", uuid).then((queryResult) => {
+        if (!queryResult.error) {
+            response.status(200).json({
+                "message": "Request OK",
+                "query": queryResult.data[0].display_name,
+            })
+        } else {
+            response.status(500).json({
+                "message": "Error. Try Again!",
+            });
+        }
+    });
+});
+
+app.listen(5501, () => { // hosting port: 5501
+    console.log("Server Session started.");
+});
+
+
+function whenSupabaseCallsMeBack(queryResult) {
+    
+}
